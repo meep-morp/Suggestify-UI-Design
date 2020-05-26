066dd61559e0e35db7fa356c36ac5205d0dbefa0
meep-morp <gracieaerogirl@gmail.com>
2020-05-26 10:27:38 -0500

    Added component files

--- spotify-login/src/components/App.jsx
+++ spotify-login/src/components/App.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 const App = () => {
+
+    const [formValues, setFormValues] = useState({})
+    const [user, setUser] = useState({})
+    const [userList, setUserList] = useState([])
+
     return (
         <div className="App">
+            <Router>
 
+            </Router>
         </div>
     )
 }
