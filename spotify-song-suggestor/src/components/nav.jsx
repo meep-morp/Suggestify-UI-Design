c8d95961856fbfab056d7f0024957d534acd4da8
meep-morp <gracieaerogirl@gmail.com>
2020-05-26 20:12:39 -0500

    added nav

--- spotify-song-suggestor/src/components/nav.jsx
+++ spotify-song-suggestor/src/components/nav.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Nav = props => {
     return (
         <nav>
-            
+            <a href="#">Our Team</a>
+            <span>|</span>
+            <a href="/signup">Sign up</a>
+            <a href="/">Log in</a>
         </nav>
     )
 }
