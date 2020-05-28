6aaf567fa61f6b0cbaff16a1e6de8c02caa23d53
meep-morp <gracieaerogirl@gmail.com>
2020-05-28 17:38:33 -0500

    Added my profile page framework

--- spotify-song-suggestor/src/components/dashboard/searchBar.js
+++ spotify-song-suggestor/src/components/dashboard/searchBar.js
@@ -1,56 +1,52 @@
-import React, { useState } from "react";
-import { axiosWithAuth } from "../utils/axiosWithAuth";
-import axios from "axios";
+import React, { useState } from 'react';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+import axios from 'axios';
 // import { axiosWithAuthDS } from '../utils/axiosWithAuthDS';
 
 const initialSearch = {
-	artist: "",
-	song: "",
-};
+    artist: '',
+    song: '',
+}
 
-const SearchBar = props => {
-	const [search, setSearch] = useState(initialSearch);
+const SearchBar = (props) => {
+    const [search, setSearch] = useState(initialSearch);
 
-	const handleChange = e => {
-		setSearch({
-			...search,
-			[e.target.name]: e.target.value,
-		});
-	};
+    const handleChange = e => {
+        setSearch({
+            ...search,
+            [e.target.name]: e.target.value
+        })
+    }
 
-	const handleSubmit = e => {
-		e.preventDefault();
-		axios
-			.post(
-				`https://spotify-3-ds.herokuapp.com/search/${search.artist}/${search.song}`
-			)
-			.then(res => {
-				document.querySelector(".songList").classList.remove("songList");
-				props.setSongs(res);
-				props.setMessage("");
-			})
-			.catch(err => props.setMessage("No results found."));
-	};
-	return (
-		<div>
-			<form className="form searchBar">
-				<input
-					type="text"
-					name="artist"
-					placeholder="Artist Name"
-					value={search.artist}
-					onChange={handleChange}
-				/>
-				<input
-					type="text"
-					name="song"
-					placeholder="Track Name"
-					value={search.song}
-					onChange={handleChange}
-				/>
-				<button onClick={handleSubmit}> SEARCH </button>
-			</form>
-		</div>
-	);
-};
-export default SearchBar;
+    const handleSubmit = e => {
+        e.preventDefault()
+        axios
+          .post(`https://spotify-3-ds.herokuapp.com/search/${search.artist}/${search.song}`)
+          .then((res) => {
+              props.setSongs(res)
+          })
+          .catch((err) => props.setSongs('Invalid Search Try Again'));
+    }
+        return(
+            <div>
+                <form>
+                        <input 
+                            type='text'
+                            name='artist'
+                            placeholder='Artist Name'
+                            value={search.artist}
+                            onChange={handleChange}
+                        />
+                        <input 
+                            type='text'
+                            name='song'
+                            placeholder='Track Name'
+                            value={search.song}
+                            onChange={handleChange}
+                        />
+                        <button onClick={handleSubmit}> Recomendations </button>
+                </form>
+            </div>
+        )
+}
+export default SearchBar
