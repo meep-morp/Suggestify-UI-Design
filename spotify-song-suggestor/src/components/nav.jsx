9107a3c206d4c57ba1a3ddafe1a17a1a6f9188ae
meep-morp <gracieaerogirl@gmail.com>
2020-05-28 15:00:19 -0500

    Starting Search Bar Styles

--- spotify-song-suggestor/src/components/nav.jsx
+++ spotify-song-suggestor/src/components/nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Nav = props => {
     return (
         <nav>
-            <a href="#">Our Team</a>
+            <a href="http://spotify-song-suggester.netlify.app">Our Team</a>
             <span>|</span>
             <a href="/signup">Sign up</a>
             <a href="/">Log in</a>
