a0ec88f6329ee499c4d61aca5cd919f90fd1d76f
meep-morp <gracieaerogirl@gmail.com>
2020-05-28 22:18:23 -0500

    Still working on Save function, added loading screen to song search

--- spotify-song-suggestor/src/components/dashboard/myProfile.jsx
+++ spotify-song-suggestor/src/components/dashboard/myProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
 import { useParams } from "react-router-dom";
+import SavedSongs from "./savedSongs";
 
 // axiosWithAuth.get(`https://spotsuggest.herokuapp.com/save/:id`)
 //     .then(response => {
@@ -13,10 +14,13 @@ import { useParams } from "react-router-dom";
 const id = useParams;
 
 const MyProfile = props => {
+    const { setSavedSongs, savedSongs } = props;
+
     const [user, setUser] = useState({});
     return (
         <div>
             <h2>Welcome Back, {user.name}</h2>
+            <SavedSongs savedSongs={savedSongs} setSavedSongs={setSavedSongs} />
         </div>
     )
 }
