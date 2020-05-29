7e852c559ba3a669cbd0fda34d3d871ff39eafed
meep-morp <gracieaerogirl@gmail.com>
2020-05-28 19:36:55 -0500

    Added an incorrect password error on 401

--- spotify-song-suggestor/src/components/dashboard/myProfile.jsx
+++ spotify-song-suggestor/src/components/dashboard/myProfile.jsx
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { useParams } from "react-router-dom";
 
-axiosWithAuth.get(`https://spotsuggest.herokuapp.com/save/:id`)
-    .then(response => {
-        setUser(response.data)
-    })
-    .catch(error => {
-        console.log(error);
-    })
+// axiosWithAuth.get(`https://spotsuggest.herokuapp.com/save/:id`)
+//     .then(response => {
+//         setUser(response.data)
+//     })
+//     .catch(error => {
+//         console.log(error);
+//     })
+
+const id = useParams;
 
 const MyProfile = props => {
     const [user, setUser] = useState({});
