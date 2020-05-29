abd805643a05daea7c396b24efd18c076454674d
Juan (programming) <programjuan@Juans-MacBook-Pro.local>
2020-05-29 13:54:46 -0400

    fixed backend/frontend stuff

--- spotify-song-suggestor/src/components/dashboard/profile/myProfile.jsx
+++ spotify-song-suggestor/src/components/dashboard/profile/myProfile.jsx
@@ -2,26 +2,30 @@ import React, { useState, useEffect } from "react";
 import { axiosWithAuth } from "../../utils/axiosWithAuth";
 import { useParams, useHistory } from "react-router-dom";
 
+import UpdateForm from './updateProfile';
+
 const MyProfile = props => {
-    const [profile, setProfile] = useState();
+    const [profile, setProfile] = useState({name: '', username: '', password: ''});
     const { id } = useParams();
     const { push } = useHistory();
 
     useEffect(() => {
         axiosWithAuth()
-            .get(`api/auth/${id}`)
+            .get(`api/auth/${localStorage.getItem('User Id')}`)
             .then(res => {
+                console.log(res.data)
                 setProfile(res.data)
             })
     }, [id])
 
     const updateProfile = e => {
-        push('/dashboard/update-profile/:id')
+        push(`/dashboard/update-profile/${localStorage.getItem('User Id')}`)
     }
 
     return (
         <div>
-            <h2>Welcome Back, User</h2>
+            <h2>Welcome Back, {profile.name}</h2>
+            <p>{profile.username}</p>
             <button onClick={updateProfile}>Update Profile</button>
         </div>
     )
--- spotify-song-suggestor/src/components/dashboard/saved-songs/ssList.js
+++ spotify-song-suggestor/src/components/dashboard/saved-songs/ssList.js
@@ -1,48 +1,52 @@
 import React from 'react';
 import { axiosWithAuth } from '../../utils/axiosWithAuth';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 
 const SavedList = (props) => {
-    const { id } = useParams()
+    const { id } = useParams();
+    const history = useHistory();
+
     const deleteSong = (song) => {
         axiosWithAuth()
-        .delete(`api/songs/${id}`)
+        .delete(`api/songs/${song.id}`)
         .then(res => {
             const undeletedSongs = props.songs.filter(song => {
-                if(song.song_id !== res.data){
+                if(song.id !== res.data){
                   return song
                 }
               })
-              props.updateSavedSongs(undeletedSongs);
+              props.updateSongs(undeletedSongs);
+              history.go(0)
         })
     }
+
     return(
         <div>
             {props.songs.map((song, id) => {
                 return (
                     <div className='song'>
                     <div className='sImg'>
-                        <img src={props.song.album_art} />
+                        <img src={song.album_art} />
                     </div>
                     <div className='sInfo'>
                         <div>
                             <h2>Song </h2> 
-                            <p>{props.song.title}</p>
+                            <p>{song.title}</p>
                         </div>
                         
                         <div>
                             <h2>Album </h2>
-                            <p>{props.song.album_name}</p>
+                            <p>{song.album_name}</p>
                         </div>
         
                         <div>
                             <h2>Artist </h2>
-                            <p>{props.song.artist}</p>
+                            <p>{song.artist}</p>
                         </div>
                         <div>
                             <button onClick={(e) => {
                                 e.stopPropagation();
-                                deleteSong(props.song)
+                                deleteSong(song)
                             }}>Delete</button>
                         </div>
                     </div>
