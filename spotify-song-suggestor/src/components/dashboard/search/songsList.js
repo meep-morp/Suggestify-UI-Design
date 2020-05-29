50c0469f11b76869139f5cf5c46e4742423760eb
meep-morp <gracieaerogirl@gmail.com>
2020-05-29 14:51:12 -0500

    Removed iFrame from buid

--- spotify-song-suggestor/src/components/dashboard/search/songsList.js
+++ spotify-song-suggestor/src/components/dashboard/search/songsList.js
@@ -1,39 +1,30 @@
-import React from 'react';
-import Song from './song';
+import React from "react";
+import Song from "./song";
 
-import './songcards.css';
+import "./songcards.css";
 
-const SongsList = (props) => {
-    let src = "src=";
-    let length = "https://plotly.com/~mherbert93/1.embed".length;
+const SongsList = props => {
+	let src = "src=";
+	let length = "https://plotly.com/~mherbert93/1.embed".length;
 
-    const convertToElement = (elmnt) => {
-        const dom = document.createElement("div");
+	const convertToElement = elmnt => {
+		const dom = document.createElement("div");
 
-        dom.classList.add("iFrame");
-        dom.innerHTML = elmnt;
+		dom.classList.add("iFrame");
+		dom.innerHTML = elmnt;
 
-        return dom
-    }
+		return dom;
+	};
 
-    return (
-        <div>
-            <div className="message">{props.message}</div>
-            <div className='cardContainer'>
-                {props.songs.map((song, id) => (
-                    (id === props.songs.length - 1)
-                        ? <div className="iframeContainer">
-                            <iframe
-                                src={song.iframe}
-                                className="igraph"
-                                scrolling="none"
-                            >
-                            </iframe>
-                        </div> :
-                        <Song key={id} song={song} />
-                ))}
-            </div>
-        </div>
-    )
-}
-export default SongsList; 
+	return (
+		<div>
+			<div className="message">{props.message}</div>
+			<div className="cardContainer">
+				{props.songs.map((song, id) => (
+					<Song key={id} song={song} />
+				))}
+			</div>
+		</div>
+	);
+};
+export default SongsList;
